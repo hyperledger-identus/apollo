c2e463d7f10ede15c92bfece0fc0a04971e3b64f
goncalo-frade-iohk <goncalo.frade@iohk.io>
2025-06-20 11:45:31 +0100

    feat: new project organization
    Signed-off-by: goncalo-frade-iohk <goncalo.frade@iohk.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,171 +1,181 @@
-val publishedMavenId: String = "org.hyperledger.identus.apollo"
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
-    id("org.jetbrains.dokka") version "1.9.20"
-    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kover)
+    alias(libs.plugins.nexus.publish)
     id("maven-publish")
-    id("org.jetbrains.kotlinx.kover") version "0.7.5"
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("signing")
-    id("com.android.library") version "8.1.4" apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
 }
 
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
-        classpath("org.jetbrains.dokka:dokka-base:1.9.20")
-    }
-}
-
-group = publishedMavenId
+group = "org.hyperledger.identus"
 
 allprojects {
-    group = publishedMavenId
+    group = rootProject.group
 
     repositories {
         google()
         mavenCentral()
     }
 
-    apply(plugin = "org.gradle.maven-publish")
-    apply(plugin = "org.gradle.signing")
-
-    publishing {
-        publications.withType<MavenPublication> {
-            groupId = publishedMavenId
-            artifactId = project.name
-            version = project.version.toString()
-            pom {
-                name.set("Identus Apollo")
-                description.set("Collection of the cryptographic methods used all around Identus platform")
-                url.set("https://docs.atalaprism.io/")
-                organization {
-                    name.set("Hyperledger")
-                    url.set("https://www.hyperledger.org/")
-                }
-                issueManagement {
-                    system.set("Github")
-                    url.set("https://github.com/hyperledger/identus-apollo")
-                }
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("hamada147")
-                        name.set("Ahmed Moussa")
-                        email.set("ahmed.moussa@iohk.io")
-                        organization.set("IOG")
-                        roles.add("developer")
-                        url.set("https://github.com/hamada147")
-                    }
-                    developer {
-                        id.set("amagyar-iohk")
-                        name.set("Allain Magyar")
-                        email.set("allain.magyar@iohk.io")
-                        organization.set("IOG")
-                        roles.add("qc")
-                    }
-                    developer {
-                        id.set("antonbaliasnikov")
-                        name.set("Anton Baliasnikov")
-                        email.set("anton.baliasnikov@iohk.io")
-                        organization.set("IOG")
-                        roles.add("qc")
-                    }
-                    developer {
-                        id.set("elribonazo")
-                        name.set("Javier Ribó")
-                        email.set("javier.ribo@iohk.io")
-                        organization.set("IOG")
-                        roles.add("developer")
-                    }
-                    developer {
-                        id.set("goncalo-frade-iohk")
-                        name.set("Gonçalo Frade")
-                        email.set("goncalo.frade@iohk.io")
-                        organization.set("IOG")
-                        roles.add("developer")
-                    }
-                    developer {
-                        id.set("curtis-h")
-                        name.set("Curtis Harding")
-                        email.set("curtis.harding@iohk.io")
-                        organization.set("IOG")
-                        roles.add("developer")
-                    }
-                    developer {
-                        id.set("cristianIOHK")
-                        name.set("Cristian Gonzalez")
-                        email.set("cristian.castro@iohk.io")
-                        organization.set("IOG")
-                        roles.add("developer")
-                    }
-                    developer {
-                        id.set("yshyn-iohk")
-                        name.set("Yurii Shynbuiev")
-                        email.set("yurii.shynbuiev@iohk.io")
-                        organization.set("IOG")
-                        roles.add("developer")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://git@github.com/hyperledger/identus-apollo.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/hyperledger/identus-apollo.git")
-                    url.set("https://github.com/hyperledger/identus-apollo")
-                }
-            }
-
-            signing {
-                useInMemoryPgpKeys(
-                    project.findProperty("signing.signingSecretKey") as String? ?: System.getenv("OSSRH_GPG_SECRET_KEY"),
-                    project.findProperty("signing.signingSecretKeyPassword") as String? ?: System.getenv("OSSRH_GPG_SECRET_KEY_PASSWORD")
-                )
-                sign(this@withType)
-            }
-        }
+    rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+        rootProject.the<YarnRootExtension>().reportNewYarnLock = true
+        rootProject.the<YarnRootExtension>().yarnLockAutoReplace = true
     }
 }
 
 subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-    apply(plugin = "org.jetbrains.kotlinx.kover")
+    if (project.name == "apollo") {
+        apply(plugin = "org.gradle.maven-publish")
+        apply(plugin = "org.gradle.signing")
 
-    koverReport {
-        filters {
-            excludes {
-                classes(
-                    "org.hyperledger.identus.apollo.utils.bip39.wordlists.*"
-                )
+        publishing {
+            publications.withType<MavenPublication> {
+                groupId = rootProject.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+
+                pom {
+                    name.set("Identus Apollo")
+                    description.set("Collection of cryptographic methods used across Identus platform.")
+                    url.set("https://docs.atalaprism.io/")
+
+                    organization {
+                        name.set("Hyperledger")
+                        url.set("https://www.hyperledger.org/")
+                    }
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("hamada147")
+                            name.set("Ahmed Moussa")
+                            email.set("ahmed.moussa@iohk.io")
+                            organization.set("IOG")
+                            roles.add("developer")
+                        }
+                        developer {
+                            id.set("amagyar-iohk")
+                            name.set("Allain Magyar")
+                            email.set("allain.magyar@iohk.io")
+                            organization.set("IOG")
+                            roles.add("qc")
+                        }
+                        developer {
+                            id.set("antonbaliasnikov")
+                            name.set("Anton Baliasnikov")
+                            email.set("anton.baliasnikov@iohk.io")
+                            organization.set("IOG")
+                            roles.add("qc")
+                        }
+                        developer {
+                            id.set("elribonazo")
+                            name.set("Javier Ribó")
+                            email.set("javier.ribo@iohk.io")
+                            organization.set("IOG")
+                            roles.add("developer")
+                        }
+                        developer {
+                            id.set("goncalo-frade-iohk")
+                            name.set("Gonçalo Frade")
+                            email.set("goncalo.frade@iohk.io")
+                            organization.set("IOG")
+                            roles.add("developer")
+                        }
+                        developer {
+                            id.set("curtis-h")
+                            name.set("Curtis Harding")
+                            email.set("curtis.harding@iohk.io")
+                            organization.set("IOG")
+                            roles.add("developer")
+                        }
+                        developer {
+                            id.set("cristianIOHK")
+                            name.set("Cristian Gonzalez")
+                            email.set("cristian.castro@iohk.io")
+                            organization.set("IOG")
+                            roles.add("developer")
+                        }
+                        developer {
+                            id.set("yshyn-iohk")
+                            name.set("Yurii Shynbuiev")
+                            email.set("yurii.shynbuiev@iohk.io")
+                            organization.set("IOG")
+                            roles.add("developer")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://git@github.com/hyperledger/identus-apollo.git")
+                        developerConnection.set("scm:git:ssh://git@github.com/hyperledger/identus-apollo.git")
+                        url.set("https://github.com/hyperledger/identus-apollo")
+                    }
+                }
+
+                signing {
+                    val signingKey =
+                        project
+                            .findProperty("signing.signingSecretKey") as String?
+                            ?: System.getenv("OSSRH_GPG_SECRET_KEY")
+                    val signingPassword =
+                        project
+                            .findProperty("signing.signingSecretKeyPassword") as String?
+                            ?: System.getenv("OSSRH_GPG_SECRET_KEY_PASSWORD")
+
+                    if (!signingKey.isNullOrEmpty() && !signingPassword.isNullOrEmpty()) {
+                        useInMemoryPgpKeys(signingKey, signingPassword)
+
+                        sign(
+                            publishing.publications.matching {
+                                gradle.startParameter.taskNames.none { it.contains("publishToMavenLocal") }
+                            }
+                        )
+                    } else {
+                        println("Signing skipped: signing keys not configured.")
+                    }
+                }
+            }
+
+            repositories {
+                mavenLocal()
             }
         }
+    } else {
+        // Explicitly disable publishing tasks for other modules
+        tasks.withType<PublishToMavenRepository>().configureEach {
+            enabled = false
+        }
+        tasks.withType<PublishToMavenLocal>().configureEach {
+            enabled = false
+        }
     }
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
 
     ktlint {
         verbose.set(true)
         outputToConsole.set(true)
         filter {
-            exclude {
-                it.file.toString().contains("external")
+            exclude { it.file.path.contains("external") }
+            exclude { it.file.path.contains("generated") }
+        }
+    }
+
+    koverReport {
+        filters {
+            excludes {
+                classes("org.hyperledger.identus.apollo.utils.bip39.wordlists.*")
             }
-            exclude {
-                it.file.toString() == "BNjs.kt" || it.file.toString() == "Curve.kt" || it.file.toString() == "PresetCurve.kt" ||
-                    it.file.toString() == "Ellipticjs.kt" || it.file.toString() == "secp256k1js.kt"
-            }
-            exclude {
-                it.file.toString().contains("external")
-                it.file.toString().contains("generated")
-            }
-            exclude { projectDir.toURI().relativize(it.file.toURI()).path.contains("/external/") }
-            exclude { projectDir.toURI().relativize(it.file.toURI()).path.contains("/generated/") }
         }
     }
 }
@@ -179,4 +189,9 @@ nexusPublishing {
             password.set(System.getenv("OSSRH_PASSWORD"))
         }
     }
+}
+
+tasks.wrapper {
+    gradleVersion = "8.13"
+    distributionType = Wrapper.DistributionType.ALL
 }
