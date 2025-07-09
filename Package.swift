1feab2f22592016f8d8c5c033707fd67bb53d56d
goncalo-frade-iohk <goncalo.frade@iohk.io>
2025-07-09 09:52:32 +0100

    ci: release swift fix package

--- Package.swift
+++ Package.swift
@@ -16,16 +16,16 @@ let package = Package(
 
     targets: [
         // LOCAL
-         .binaryTarget(
-             name: "ApolloBinary",
-             path: "./apollo/build/packages/ApolloSwift/Apollo.xcframework.zip"
-         ),
+//          .binaryTarget(
+//              name: "ApolloBinary",
+//              path: "./apollo/build/packages/ApolloSwift/Apollo.xcframework.zip"
+//          ),
 
         // RELEASE
-//        .binaryTarget(
-//            name: "ApolloBinary",
-//            url: "https://github.com/hyperledger-identus/apollo/releases/download/v1.7.1/Apollo.xcframework.zip",
-//            checksum: "0ce47f2c72a9561abcb9c9edd43a5442731938d48e99a6032fea9ae466795e0b"
-//        )
+       .binaryTarget(
+           name: "ApolloBinary",
+           url: "https://github.com/hyperledger-identus/apollo/releases/download/v1.7.1/Apollo.xcframework.zip",
+           checksum: "0ce47f2c72a9561abcb9c9edd43a5442731938d48e99a6032fea9ae466795e0b"
+       )
     ]
 )
