5c91310db916d118dcb629462bfcc2f4594a46bb
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-07-11 16:32:09 +0800

    ci: add signing plugin
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.nexus.publish)
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    signing
 }
 
 group = "org.hyperledger.identus"
